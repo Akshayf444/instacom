e8699615c50ef3e2022a0ad22bd374c54635b94b
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-11-26 15:02:35 +0530

    added registration

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'shortenurl';
+$db['default']['hostname'] = '50.62.209.81';
+$db['default']['username'] = 'instacom';
+$db['default']['password'] = 'instacom@123';
+$db['default']['database'] = 'instacom';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
